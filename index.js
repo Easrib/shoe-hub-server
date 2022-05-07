ceffe2ab2ff991a546d74265e17207d4e4fef157
Easrib <easrib.sharik01@gmail.com>
2022-05-07 18:17:12 +0600

    mongo db connected

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bjph8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('db connect');
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello from Shoe Hub apps')
